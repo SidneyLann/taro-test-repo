4188ddeb32e95b056ca307b59be17d1109d3d52d
Administrator <Administrator@www.pc9g.com>
2023-03-24 18:16:58 +0800

    gg

--- taro-test/src/pages/index/index.tsx
+++ taro-test/src/pages/index/index.tsx
@@ -1,9 +1,10 @@
 import { Component, PropsWithChildren } from 'react'
+import Taro from '@tarojs/taro'
+import { View, Button } from '@tarojs/components'
 import CheckBoxes from './CheckBoxes.tsx'
 import './index.scss'
 
 export default class Index extends Component<PropsWithChildren> {
-
     constructor(props) {
         super(props)
         this.state = {
@@ -18,6 +19,51 @@ export default class Index extends Component<PropsWithChildren> {
 
   componentDidHide () { }
 
+   uploadSingleFile = (file, succ, err) => {
+      if (file.size > 10 * 1024 * 1024) {
+          throw Error('文件不能大于10兆');
+      }
+
+      const header = {}
+      header['content-type'] = 'multipart/form-data'
+      header['token'] = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI3NDMwMjE5NTY4NjQ5MzM4ODkiLCJhdXRob3JpdGllcyI6WyIxMjExMTEiLCI5MDAwMDQiLCIxMTk4MTQiLCIxMTMwMTEiLCIxMDAwMDQiLCIxMTMwNTEiLCIxMjE4MTQiLCIxMDAwMDgiXSwiaXNzIjoicGNuZy1zcnYtYXV0aCIsImF1ZCI6IndlYiIsImlhdCI6MTY3OTUzNTM5MSwiZXhwIjoxNjgwMzk5MzkxfQ.LlgV8loR8bTyWwcN4gPc-kDysugoQSuotKOIWW_G3RHfz8NsXvFKJvOVN7He5t7Pr-QYmcdZgmlwbi8ODsMzlQ"
+
+      const params = {
+          url: "https://www.pc8g.com/pcng-biz-member/sys/file/upload",
+          name: 'uploadFile',
+          header: header,
+          filePath: file.path,
+          success: succ,
+          fail: err
+      }
+
+      console.log('params');
+      console.log(params);
+      Taro.uploadFile(params);
+  }
+  
+  chooseImage = (event) => {
+      Taro.chooseImage({
+          count: 10,
+          success: (result) => {
+              const images = result.tempFiles;
+              images.forEach((item, idx) => {
+                  const succ = (result) => {
+                      console.log('succ:')
+                      console.log(result)
+                  }
+
+                  const err = (result) => {
+                      console.log('err:')
+                      console.log(result)
+                  }
+
+                  this.uploadSingleFile(item, succ, err);
+              })
+          }
+      })
+  }
+  
 	onCheckBoxesChanged = (name, selectedValues) => {
 		//this.setState({ [name]: selectedValues });
 		this.setState({ "checkedList": selectedValues });
@@ -32,12 +78,15 @@ export default class Index extends Component<PropsWithChildren> {
     const checkedList = this.state.checkedList
 	console.log(checkedList)
 	
-    return (
+    return (<View>
+        <Button onClick={this.chooseImage}>+</Button>
+        
 		<CheckBoxes name='checkedList'
 					options={options}
 				    value={checkedList}
 				    onChange={this.onCheckBoxesChanged}
-				/>
+	     />
+         </View>
     )
   }
 }
