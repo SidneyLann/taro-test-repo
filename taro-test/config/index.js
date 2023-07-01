fa7011eb0d14e105b2cb0458807c65a32312a806
Administrator <Administrator@tech8>
2023-07-01 22:58:22 +0800

    gg

--- taro-test/config/index.js
+++ taro-test/config/index.js
@@ -25,7 +25,7 @@ const config = {
   framework: 'react',
   compiler: 'webpack5',
   cache: {
-    enable: true // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
+    enable: true
   },
   mini: {
     webpackChain(chain, webpack) {
