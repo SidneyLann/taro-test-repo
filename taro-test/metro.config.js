87329de81ec621f036f3987d5f554cf29077e73a
Administrator <Administrator@tech8>
2023-08-17 13:02:02 +0800

    gg

--- taro-test/metro.config.js
+++ taro-test/metro.config.js
@@ -2,7 +2,7 @@ const { mergeConfig } = require('metro-config')
 const { getMetroConfig } = require('@tarojs/rn-supporter')
 
 module.exports = (async function (){
-  return mergeConfig({
+  return mergeConfig({ 
   // custom your metro config here
   // https://facebook.github.io/metro/docs/configuration
     resolver: {}
